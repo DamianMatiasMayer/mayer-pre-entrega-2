e728c9bae4ab718a7a7c86a60ef3926c116baabe
Damian Mayer <damian.matias.mayer@hotmail.com>
2023-06-26 19:38:58 -0300

    ajustes

--- src/componentes/inicio/index.js
+++ src/componentes/inicio/index.js
@@ -2,17 +2,17 @@ import React from 'react'
 import Portada from "../../images/inicio.jpg"
 import  { Link } from "react-router-dom"
 
- const Inicio = () => {
+const Inicio = () => {
   return (
-    <div className='inicio'>
-      <Link to="./">
-        <h1 className='title'>Inicio</h1>
-      </Link>
-      <Link to="/productos">
-        <h1 className='title'>Productos</h1>
-      </Link>
-    </div>
+  <div className='inicio'>
+  <Link to="./">
+  <h1 className='title'>Inicio</h1>
+  </Link>
+  <Link to="/productos">
+    <h1 className='title'>Productos</h1>
+  </Link>  
+  </div>
   )
-}
-
-export default Inicio
+  }
+  
+  export default Inicio
