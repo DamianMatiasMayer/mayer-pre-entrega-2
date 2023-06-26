eaa8995f62d11a05fe4203f0f836928d516ea212
Damian Mayer <damian.matias.mayer@hotmail.com>
2023-06-26 19:26:51 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header } from "./componentes/Header";
 import 'boxicons';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductosLista from './componentes/Productos';
-import { Paginas } from "./componentes/Paginas/";
+
 
 
 
@@ -13,11 +13,11 @@ import { Paginas } from "./componentes/Paginas/";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Header/> }  /> 
-        <Route path='/Productos' element={<ProductosLista/>} />
-      </Routes>
-    <BrowserRouter/>
+            <Routes>
+                <Route path='/' element={<Header />} />
+                <Route path='/Productos' element={<ProductosLista />} />
+            </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/componentes/inicio/index.js
+++ src/componentes/inicio/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Portada from "../../images/inicio.jpg"
+import  { Link } from "react-router-dom"
 
-
- export const Inicio = () => {
+ const Inicio = () => {
   return (
-    <div>index</div>
+    <div className='inicio'>
+      <Link to="./">
+        <h1 className='title'>Inicio</h1>
+      </Link>
+      <Link to="/productos">
+        <h1 className='title'>Productos</h1>
+      </Link>
+    </div>
   )
 }
 
